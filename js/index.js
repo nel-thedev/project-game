043ea5457c2ed21fb37b1ac176f99dfe3e742b81
nel-thedev <nelelias@gmail.com>
2023-04-06 19:46:49 -0400

    add more script

--- js/index.js
+++ js/index.js
@@ -2,10 +2,56 @@
 
 const game = document.getElementById('game');
 const ctx = game.getContext('2d');
+let circlesArr = [];
+// let bpm = 0;
+let speed = 1;
+let colorBlueOrRed = true; //true = blue, false = red
+
+
 
 class Circle {
     constructor(position) {
-        x = 
+        this.x = randomPos();
+        this.y = 0;
+        this.dia = 22.5;
+    }
+
+    updatePos() {
+        this.y += 8.8 * speed;
+        //maybe add some way of relating this to bpm, but the circles have to reach the line with the bpm intead of being generated with bpm
+    }
+
+    drawCircle() {
+
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, (this.dia / 2), 0, Math.PI * 2);
+
+        if (colorBlueOrRed) {
+            ctx.fillStyle = "rgb(135,206,250)";
+        } else {
+            ctx.fillStyle = "rgb(220,20,60)";
+        };
+
+        ctx.fill();
+        ctx.closePath();
+    }
+}
+
+function checkCollision(circle){
+    if (collides300){
+        score += 300;
+        count300++;
+
+    } else if (collides100){
+        score += 100;
+        count100++;
+
+    } else if (collides50){
+        score += 50;
+        count50++
+
+    } else if (passes the timing window){
+        
     }
 }
 
@@ -14,24 +60,24 @@ function randomPos() {
 
     switch (true) {
         case (0 <= rnd < 0.25): {
-            return 'a'
+            return 50;
             break;
         }
         case (0.25 <= rnd < 0.50): {
-            return 'b'
+            return 150;
             break;
         }
         case (0.50 <= rnd < 0.75): {
-            return 'c'
+            return 250;
             break;
         }
         case (0.75 <= rnd <= 1): {
-            return 'd'
+            return 350;
             break;
         }
     }
 }
 
-function appearCircle() {
-    draw(randomPos())
-}
+// function appearCircle() {
+    
+// }
