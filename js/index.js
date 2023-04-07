343c1fd6abcaec5cdefa96d7a55f547cd6598927
unknown <nelelias@gmail.com>
2023-04-06 21:20:38 -0400

    add visual timing window with color change

--- js/index.js
+++ js/index.js
@@ -3,17 +3,35 @@
 const game = document.getElementById('game');
 const ctx = game.getContext('2d');
 let circlesArr = [];
-// let bpm = 0;
+let bpm = 180;
 let speed = 1;
-let colorBlueOrRed = true; //true = blue, false = red
+let circleColorBlueOrRed = true; //true = blue, false = red
+let playerColorBlueOrRed = true;
+game.height = 650;
+game.width = 400;
+
+
+//keys pressed:
+dKey = false;
+fKey = false;
+jKey = false;
+kKey = false;
+
+//counters:
+let count300;
+let count100;
+let count50;
+let countMiss;
+let combo = 0;
 
 
 
 class Circle {
-    constructor(position) {
+    constructor() {
         this.x = randomPos();
         this.y = 0;
         this.dia = 22.5;
+        this.color = circleColorBlueOrRed;
     }
 
     updatePos() {
@@ -26,7 +44,7 @@ class Circle {
         ctx.beginPath();
         ctx.arc(this.x, this.y, (this.dia / 2), 0, Math.PI * 2);
 
-        if (colorBlueOrRed) {
+        if (circleColorBlueOrRed) {
             ctx.fillStyle = "rgb(135,206,250)";
         } else {
             ctx.fillStyle = "rgb(220,20,60)";
@@ -35,25 +53,47 @@ class Circle {
         ctx.fill();
         ctx.closePath();
     }
+    
 }
 
-function checkCollision(circle){
-    if (collides300){
-        score += 300;
-        count300++;
+function generateCircle(){
+    setInterval(() => {
+        circlesArr.push(new Circle());
+        console.log('new circle');
+    }, (60000 / bpm));
+}
 
-    } else if (collides100){
-        score += 100;
-        count100++;
+function clearCircle(){
+    circlesArr.shift()
+    console.log('click!')
+}
 
-    } else if (collides50){
-        score += 50;
-        count50++
+// function checkCollision(circle){
+//     if (collides300 && same color){
+//         score += 300;
+//         count300++;
+//         combo++;
+//         clearCircle()
 
-    } else if (passes the timing window){
+//     } else if (collides100 && same color){
+//         score += 100;
+//         count100++;
+//         combo++;
+//         clearCircle()
+
+//     } else if (collides50 && same color){
+//         score += 50;
+//         count50++;
+//         combo++;
+//         clearCircle()
+
+//     } else if (passes the timing window || is in timing wnd but diff color){
+//         countMiss++;
+//         combo = 0;
+//         clearCircle()
         
-    }
-}
+//     }
+// }
 
 function randomPos() {
     const rnd = Math.random()
@@ -75,6 +115,96 @@ function randomPos() {
             return 350;
             break;
         }
+    }
+}
+
+
+document.addEventListener('keydown', e => {
+    switch (e.keyCode) {
+        case 68:
+            dKey = true;
+            console.log('d pressed')
+            break;
+    
+        case 70:
+            fKey = true;
+            console.log('f pressed')
+            break;
+
+        case 74:
+            jKey = true;
+            console.log('j pressed')
+            break;
+        
+        case 75:
+            kKey = true;
+            console.log('k pressed')
+            break;
+
+        case 32:
+            playerColorBlueOrRed = !playerColorBlueOrRed;
+            break;
+    }
+})
+
+document.addEventListener('keyup', e => {
+    switch (e.keyCode) {
+        case 68:
+            dKey = false;
+            console.log('d unpressed')
+            break;
+    
+        case 70:
+            fKey = false;
+            console.log('f unpressed')
+            break;
+
+        case 74:
+            jKey = false;
+            console.log('j unpressed')
+            break;
+        
+        case 75:
+            kKey = false;
+            console.log('k unpressed')
+            break;
+    }
+})
+
+function animate() {
+    ctx.clearRect(0,0,game.width,game.height)
+
+    // timing window:
+    if (playerColorBlueOrRed) {
+        ctx.fillStyle = "rgba(135,206,250, 0.1)";
+        ctx.fillRect(0,520,game.width,100);
+        ctx.fillRect(0,535,game.width,70);
+        ctx.fillRect(0,550,game.width,40);
+        ctx.fillRect(0,569,game.width,1);
+    } else {
+        ctx.fillStyle = "rgba(220,20,60, 0.1)";
+        ctx.fillRect(0,520,game.width,100);
+        ctx.fillRect(0,535,game.width,70);
+        ctx.fillRect(0,550,game.width,40);
+        ctx.fillRect(0,569,game.width,1);
+    };
+    // need to draw stuff here
+
+
+    requestAnimationFrame(animate);
+}
+
+function startGame(){
+    animate();
+    console.log('Starting...');
+
+    // maybe add an offset kinda delay here:
+    generateCircle();
+}
+
+window.onload = () => {
+    document.getElementById('start-btn').onclick = () => {
+        startGame()
     }
 }
 
